e139cff03565c13a51dff30d8604fbd697a55920
Vbubblery <juncheng.zhou@aizimov.com>
2019-09-12 11:27:03 +0200

    delete test

--- src/tests/graphql/product/delete.ts
+++ src/tests/graphql/product/delete.ts
@@ -7,10 +7,13 @@ export const deleteProductGraphqlTest = () =>
   describe("Test product delete Graphql", () => {
     beforeAll(async () => {
       await init("test_db");
-      await new Product({
+      const product: Product = await new Product({
         productId: "2b01dbe3-4b0b-4baf-8ed5-58e818a0a8ed",
         productName: "test"
       }).save();
+      await product.addNewData({ date: 1567695422881, inventoryLevel: 1 });
+      await product.addNewData({ date: 1567695422882, inventoryLevel: 2 });
+      await product.addNewData({ date: 1567695422883, inventoryLevel: 3 });
       await new Product({
         productName: "test2"
       }).save();
@@ -22,6 +25,42 @@ export const deleteProductGraphqlTest = () =>
       jDestroy();
     });
     afterEach(async () => {});
+
+    test("delete a data by product id", async () => {
+      const mutation = `
+        mutation{
+          deleteDataFromProductParse(
+            productId: "2b01dbe3-4b0b-4baf-8ed5-58e818a0a8ed"
+            date: 1567695422881
+          ) {
+            status
+          }
+        }
+      `;
+
+      const response = <any>await graphql(schema, mutation);
+      expect(response.data.deleteDataFromProductParse.status).toBe(
+        `Successful`
+      );
+    });
+
+    test("delete a data by product name", async () => {
+      const mutation = `
+        mutation{
+          deleteDataFromProductParse(
+            productName: "test"
+            date: 1567695422882
+          ) {
+            status
+          }
+        }
+      `;
+
+      const response = <any>await graphql(schema, mutation);
+      expect(response.data.deleteDataFromProductParse.status).toBe(
+        `Successful`
+      );
+    });
 
     test("delete a product by id", async () => {
       const mutation = `mutation {
